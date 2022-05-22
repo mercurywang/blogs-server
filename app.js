b1df9b86c21823d6714b40cd93a872c5ab38bd15
mercury_wang <mercwang@hotmail.com>
2022-05-22 18:53:47 +0800

    init: dev

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controller/blogs')
+
+const mongoUrl = config.MONGODB_URI
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
