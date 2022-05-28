db6b64db477fa54abb8fb31b4e65f7a0501f9653
mercury_wang <mercwang@hotmail.com>
2022-05-28 15:18:19 +0800

    feat: ex 4.15

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 
 const mongoUrl = config.MONGODB_URI
 mongoose
@@ -20,5 +21,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
