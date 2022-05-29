b9639af1aac5d82bca4b72b713922b8c8d62e7fc
mercury_wang <mercwang@hotmail.com>
2022-05-29 13:28:48 +0800

    feat: ex 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, author, url, likes = 0 } = request.body
+  const { title, author, url, likes = 0, userId } = request.body
+
+  const user = await User.findById(userId)
 
   if (!title && !url) {
     response.status(400).end()
@@ -18,9 +20,12 @@ blogsRouter.post('/', async (request, response) => {
       author,
       url,
       likes,
+      user: user._id,
     })
 
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
 
     response.status(201).json(result)
   }
